5170c732a72ab12b27cac54ef33a3759bb77cd35
mlukacs <mlukacs@deezer.com>
2021-04-12 18:50:42 +0200

    Initial Commit

---
+++ FoodFinder/FoodFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFinderApp.swift
+//  FoodFinder
+//
+//  Created by Martin Lukacs on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
