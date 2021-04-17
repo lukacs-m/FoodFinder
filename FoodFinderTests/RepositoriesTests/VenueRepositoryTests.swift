df642c59f2aa11a1f922fbb2ef5133f22bf3741c
mlukacs <mlukacs@deezer.com>
2021-04-17 11:48:39 +0200

    finished clean of test

--- FoodFinderTests/RepositoriesTests/VenueRepositoryTests.swift
+++ FoodFinderTests/RepositoriesTests/VenueRepositoryTests.swift
@@ -18,18 +18,18 @@ class VenueRepositoryTests: XCTestCase {
     override func setUpWithError() throws {
         Resolver.resetUnitTestRegistrations()
     }
-
+    
     ///  Example of how to test functions from repository
     ///  We are testing one of the calls used by viewmodels to get details venue data
     /// - Throws: Error if not the dummy test data that is checked
     func testGetDetailedVenue() throws {
-
-       Resolver.test.register { VenueAPIDetailVenuMock() as VenueAPIContract }.scope(.shared)
-
+        
+        Resolver.test.register { VenueAPIDetailVenuMock() as VenueAPIContract }.scope(.shared)
+        
         let sut = VenueRepository()
         let publisher = sut.getDetails(ofType: DetailedVenue.self,
-                                     for: ModelsMockFactory.getMapVenuAnnotionDummy())
-
+                                       for: ModelsMockFactory.getMapVenuAnnotionDummy())
+        
         let publisherStreamResponse = try await(publisher)
         XCTAssertNotNil(publisherStreamResponse)
         XCTAssert(publisherStreamResponse?.id == ModelsMockFactory.getDetailedVenueDummy().id)
